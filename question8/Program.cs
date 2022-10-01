f5c060d631040e87d467ddcac32fe68cf12dd478
Shilpa.Devanga <Shilpa.Devanga@INLPF2KZ387>
2022-10-01 08:54:59 +0530

    Add project files.

---
+++ question8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System;
+
+namespace question8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your name");
+            string name = Console.ReadLine();
+            string fileName = @"C:\Users\shilpa.devanga\\file1" + name + ".txt";
+            try
+            {
+                StreamWriter sw = File.AppendText(fileName);
+
+
+
+                sw.WriteLine("1st line added");
+                sw.WriteLine("adding more text");
+                sw.WriteLine("again adding text");
+                sw.WriteLine("further adding text");
+                sw.WriteLine("adding texttttt");
+                sw.WriteLine("adding texttttt");
+                sw.WriteLine("adding texttttt");
+                sw.WriteLine("adding texttttt");
+                sw.WriteLine("adding texttttt");
+                sw.WriteLine("adding texttttt");
+                sw.WriteLine("adding texttttt");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Enter the text you wanna replaced");
+            string OldText = Console.ReadLine();
+            Console.WriteLine("Enter the text you want in the file");
+            string NewText = Console.ReadLine();
+            string text = File.ReadAllText(fileName);
+            text = text.Replace(OldText, NewText);
+            File.WriteAllText(fileName, text);
+            Console.ReadKey();
+        }
+    }
+}
